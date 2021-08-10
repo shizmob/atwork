aa143888cf39fcd3668d76bac6aeaafb8b7a8858
Shiz <hi@shiz.me>
2021-08-10 23:58:31 +0200

    build: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='atwork',
+    version='0.1.0',
+    author='Shiz <hi@shiz.me>',
+    url='https://github.com/Shizmob/atwork',
+    description='easy progress indication UI library',
+
+    py_modules=['atwork'],
+    zip_safe=True,
+)
